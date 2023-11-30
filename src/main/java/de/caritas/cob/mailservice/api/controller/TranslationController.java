91f114b2c57da51370e84e8b8c71720daf23afa4
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-11-30 11:47:34 +0100

    fix: remove not needed endpoint

--- src/main/java/de/caritas/cob/mailservice/api/controller/TranslationController.java
+++ src/main/java/de/caritas/cob/mailservice/api/controller/TranslationController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -20,12 +19,6 @@ public class TranslationController {
   @GetMapping(value = "/translations")
   public ResponseEntity<Map<String, String>> getTranslations() {
     var result = translationService.fetchTranslations("de");
-    return new ResponseEntity<>(result, org.springframework.http.HttpStatus.OK);
-  }
-
-  @GetMapping(value = "/translations/{lang}")
-  public ResponseEntity<Map<String, String>> getTranslations(@PathVariable("lang") String languageCode) {
-    var result = translationService.fetchTranslations(languageCode);
     return new ResponseEntity<>(result, org.springframework.http.HttpStatus.OK);
   }
 
